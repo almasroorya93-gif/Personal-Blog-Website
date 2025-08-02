d96fec7777416b91c33a85823c8cb73d6a774232
almasroorya93-gif <almasroorya93@gmail.com>
2025-08-02 19:45:26 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// primitive data Types
+const name ='John Doe';
+const age = 45;
+const ieMale= true;
+const height =5.9;
+console.log ("*****primitive data Types******");
+console.log(name);
+console.log(age);
+console.log(ieMale);
+console.log( height);
+
+
+console.log ("*****primitive data Types******");
+console.log(myArray);
+console.log(myColors);
+console.log(myBools);
+
+console.log ("***** accessing array elements ******");
+console.log(myColors[0]);
+console.log(myColors[1]);
+console.log(myColors[2]);
+
+console.log ("***** checking array methods******");
+console.log(myColors[0]);
+
+function forMyArray
+arrayPrint
